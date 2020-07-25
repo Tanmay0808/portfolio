335c4f824a192a7c9c1c95a50db07d62cf5550b2
Tanmay0808 <tanmay881999@gmail.com>
2020-07-25 10:46:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
+import About from './components/About';
+import Education from './components/Education';
+import Projects from "./components/Projects";
+import Contact from './components/Contact';
+import Footer from "./components/Footer";
+import "./styles/index.scss";
+import "../node_modules/aos/dist/aos.css";
+import AOS from "aos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    AOS.init();
+  }
+
+  componentDidMount() {
+    var btn = document.getElementById('go-top');
+
+    //Navbar Initially Hidden
+    var navbar = document.getElementById('navbar');
+    navbar.style.opacity = 0;
+    navbar.style.transition = 'all .4s linear';
+
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 620 || document.documentElement.scrollTop > 620) {
+        btn.style.display = 'block';
+        navbar.style.opacity = 1;
+      }
+      else {
+        btn.style.display = 'none';
+        navbar.style.opacity = 0;
+      }
+    }
+
+    btn.addEventListener('click', () => {
+      document.body.scrollTop = 0; //Safari
+      document.documentElement.scrollTop = 0;//For Chrome ,Firefox, IE & Opera
+    });
+  }
+
+  componentWillReceiveProps() {
+    AOS.refresh();
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Intro />
+        <About />
+        <Education />
+        <Projects />
+        <Contact />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
